d3af1dcc5a696a6675892d79a892c5d7f950150c
Aaron <supermanaaronliu@gmail.com>
2019-02-06 22:30:05 -0700

    add dirt

---
+++ src/Scripts/Dirt.gd
@@ -0,0 +1,27 @@
+extends StaticBody2D
+ 
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+    # Called when the node is added to the scene for the first time.
+    # Initialization here
+    pass
+
+var PLAYER_ON_ME = false
+
+func _on_Area2D_body_entered(body):
+    if body.is_in_group("player"):
+#        print("player comes")
+        PLAYER_ON_ME = true
+
+func _on_Area2D_body_exited(body):
+    if body.is_in_group("player"):
+#        print("player goes")
+        PLAYER_ON_ME = false
+        
+func _physics_process(delta):
+    #TODO: add condition: only check this if player form is FOX
+    if PLAYER_ON_ME and Input.is_mouse_button_pressed(BUTTON_LEFT):
+        queue_free()
