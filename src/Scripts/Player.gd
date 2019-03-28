3741a7af8ca39f8420be1152fd8805848e903ac3
Atharv Vohra <atharv@ualberta.ca>
2019-03-27 19:40:53 -0600

    added respawn delay, fixed some ui elements

--- src/Scripts/Player.gd
+++ src/Scripts/Player.gd
@@ -23,104 +23,105 @@ var JUMPING = false
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	pass
+	$DeathTimer.start()
 	
 func _physics_process(delta):
+	
+	if Global.CAN_MOVE == true:
 		
-	if Input.is_key_pressed(KEY_R):
-		respawn()
-	
-	if Input.is_action_pressed("ui_right"):
-		$Basic.set_flip_h(false) 
-		if animPlay == false:
-			$AnimationPlayer.play("FoxRun")
-			animPlay = true
-		if xspeed < 0:
-			xspeed += 3 * ACC
+		if Input.is_key_pressed(KEY_R):
+			respawn_R()
+		
+		if Input.is_action_pressed("ui_right"):
+			$Basic.set_flip_h(false) 
+			if animPlay == false:
+				$AnimationPlayer.play("FoxRun")
+				animPlay = true
+			if xspeed < 0:
+				xspeed += 3 * ACC
+			else:
+				xspeed += ACC
+				if xspeed > MAX_SPEED:
+					xspeed = MAX_SPEED
+		
+		elif Input.is_action_pressed("ui_left"):
+			$Basic.set_flip_h(true)
+			if animPlay == false:
+				$AnimationPlayer.play("FoxRun")
+				animPlay = true
+			if xspeed > 0:
+				xspeed -= 3 * ACC
+			else:
+				xspeed -= ACC
+				if xspeed < -MAX_SPEED:
+					xspeed = -MAX_SPEED
+					
 		else:
-			xspeed += ACC
-			if xspeed > MAX_SPEED:
-				xspeed = MAX_SPEED
-	
-	elif Input.is_action_pressed("ui_left"):
-		$Basic.set_flip_h(true)
-		if animPlay == false:
-			$AnimationPlayer.play("FoxRun")
-			animPlay = true
-		if xspeed > 0:
-			xspeed -= 3 * ACC
-		else:
-			xspeed -= ACC
-			if xspeed < -MAX_SPEED:
-				xspeed = -MAX_SPEED
+			$AnimationPlayer.stop()
+			animPlay = false
+			if xspeed > ((ACC * 2) + 1):
+				xspeed -= 2 * ACC
+			elif xspeed < -16:
+				xspeed += 2 * ACC
+			else:
+				xspeed = 0
 				
-	else:
-		$AnimationPlayer.stop()
-		animPlay = false
-		if xspeed > ((ACC * 2) + 1):
-			xspeed -= 2 * ACC
-		elif xspeed < -16:
-			xspeed += 2 * ACC
-		else:
-			xspeed = 0
-			
-	if Input.is_action_pressed("ui_lmb"):
-		if not is_on_floor() and landed == true and Global.DASH_ENABLED:
-			dash = true
+		if Input.is_action_pressed("ui_lmb"):
+			if not is_on_floor() and landed == true and Global.DASH_ENABLED:
+				dash = true
+		
+		if is_on_floor():
+			if Input.is_action_just_pressed("space"):
+				$jumpsound1.play() 
+				yspeed = jforce		
 	
-	if is_on_floor():
-		if Input.is_action_just_pressed("space"):
-			$jumpsound1.play() 
-			yspeed = jforce		
-
-	# Dashing code
-	if dash == true:
+		# Dashing code
+		if dash == true:
+				
+			landed = false
+			if xspeed > 0:
+				xspeed = 900
+				yspeed = 0
+				count += 1
+			elif xspeed < 0:
+				xspeed = -900
+				yspeed = 0
+				count += 1
+			else:
+				dash = false
+		
+		if not is_on_floor() and dash == false:
+			JUMPING = true
 			
-		landed = false
-		if xspeed > 0:
-			xspeed = 900
-			yspeed = 0
-			count += 1
-		elif xspeed < 0:
-			xspeed = -900
-			yspeed = 0
-			count += 1
-		else:
+			if yspeed > 0:
+				yspeed -= 24
+			elif yspeed <= 0:
+	#			ACC = 200
+				MAX_SPEED = 550
+				yspeed -= 15
+				
+		if count >= 15:
 			dash = false
-	
-	if not is_on_floor() and dash == false:
-		JUMPING = true
-		
-		if yspeed > 0:
-			yspeed -= 24
-		elif yspeed <= 0:
-#			ACC = 200
-			MAX_SPEED = 550
-			yspeed -= 15
+			count = 0
 			
-	if count >= 15:
-		dash = false
-		count = 0
+		if is_on_floor() and JUMPING:
+			ACC = 100
+			MAX_SPEED = 530
+			JUMPING = false
+			
+		if is_on_floor():
+			landed = true
 		
-	if is_on_floor() and JUMPING:
-		ACC = 100
-		MAX_SPEED = 530
-		JUMPING = false
-		
-	if is_on_floor():
-		landed = true
+		#print(xspeed, yspeed)
+		velocity.x = 1 * xspeed
+		# control falling speed
+		velocity.y = min(-1 * yspeed, 2000)
 	
-	#print(xspeed, yspeed)
-	velocity.x = 1 * xspeed
-	# control falling speed
-	velocity.y = min(-1 * yspeed, 2000)
-
-	move_and_slide(velocity, UP)
+		move_and_slide(velocity, UP)
 	
 
-func respawn():
+func respawn_R():
 	Global.respawn_player()
 	
-
-
-
+func _on_DeathTimer_timeout():
+	Global.CAN_MOVE = true
